5b7d28cedcd2395864fabb5691cbf81ab8bddcfc
Haribhusal <bhusalhari89@gmail.com>
2025-04-15 21:36:40 +0545

    project + counter

---
+++ projects/portfolio-html-css/assets/script.js
@@ -0,0 +1,6 @@
+let toggleIcon = document.getElementById('toggle');
+let menu = document.getElementById('mobileMenu');
+
+toggleIcon.addEventListener('click', function () {
+    menu.classList.toggle('active')
+})
